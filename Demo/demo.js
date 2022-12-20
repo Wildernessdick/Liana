093ba098ab678218d62cde8c675500c4ec17b2db
Simo <76773814+Hovari@users.noreply.github.com>
2022-12-20 19:00:39 +0200

    counters are now up and running.

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,3 +1,21 @@
 function newsLetter() {
   alert("Thank you for ordering our newsletter!");
 }
+/* Js animated numbers */
+let counts = setInterval(countClientes);
+let clients = 2820; //3000
+let employess = 0; //180
+let users = 820; //1000
+
+function countClientes() {
+  var clientCount = document.getElementById("clientCounter");
+  var etCount = document.getElementById("employessCounter");
+  var uCount = document.getElementById("usersCounter");
+  employessCounter.innerHTML = ++employess;
+  usersCounter.innerHTML = ++users;
+  clientCount.innerHTML = ++clients;
+
+  if (clients === 3000) {
+    clearInterval(counts);
+  }
+}
