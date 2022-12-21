86f04abf5a1c8e7b9ed3ba90639b95b2b0191ac0
Simo <76773814+Hovari@users.noreply.github.com>
2022-12-21 16:37:06 +0200

    problem with rss parse. minor changes to layout.

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,6 +1,7 @@
 function newsLetter() {
   alert("Thank you for ordering our newsletter!");
 }
+
 /* Js animated numbers */
 let counts = setInterval(countClientes);
 let clients = 2820; //3000
@@ -9,8 +10,8 @@ let users = 820; //1000
 
 function countClientes() {
   var clientCount = document.getElementById("clientCounter");
-  var etCount = document.getElementById("employessCounter");
-  var uCount = document.getElementById("usersCounter");
+  // var etCount = document.getElementById("employessCounter");
+  // var uCount = document.getElementById("usersCounter");
   employessCounter.innerHTML = ++employess;
   usersCounter.innerHTML = ++users;
   clientCount.innerHTML = ++clients;
