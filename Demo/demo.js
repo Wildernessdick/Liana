ec6e9d3dded92775087e954b4d27ebba12304835
Simo <76773814+Hovari@users.noreply.github.com>
2022-12-22 03:53:39 +0200

    had problems with rss, workaround with json.

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,22 +1,59 @@
+/* i think alarmbox is bit anoing ? */
 function newsLetter() {
   alert("Thank you for ordering our newsletter!");
 }
 
-/* Js animated numbers */
-let counts = setInterval(countClientes);
-let clients = 2820; //3000
-let employess = 0; //180
-let users = 820; //1000
-
-function countClientes() {
-  var clientCount = document.getElementById("clientCounter");
-  // var etCount = document.getElementById("employessCounter");
-  // var uCount = document.getElementById("usersCounter");
-  employessCounter.innerHTML = ++employess;
-  usersCounter.innerHTML = ++users;
-  clientCount.innerHTML = ++clients;
-
-  if (clients === 3000) {
-    clearInterval(counts);
+function validateForm() {
+  let x = document.forms["newsletter"]["email"].value;
+  if (x == "") {
+    alert("give your email");
+  } else if (x.includes("@") && x.includes(".")) {
+    location.href = "Demo/newsletter.html";
+    return;
+  } else {
+    alert("Email is not correct");
+    return false;
   }
 }
+
+/* --Fetch-- */
+/* Title */
+/* Link */
+/* generator */
+/* description */
+/* language */
+/* pubDate */
+/* öastBioödDate */
+/* ttl */
+/* --Fetch-- */
+
+// I had problems with rss, so i hope my json workaround it's ok...
+
+window.onload = async function () {
+  let url =
+    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.lianatech.com%2Fresources%2Fblog.rss";
+  var arr = [];
+
+  await fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      arr.push(data.items[0]);
+      arr.push(data.items[1]);
+      arr.push(data.items[2]);
+    })
+
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  try {
+    document.getElementById("news1date").innerHTML = arr[0].pubDate;
+    document.getElementById("news1title").innerHTML = arr[0].title;
+    document.getElementById("news2date").innerHTML = arr[1].pubDate;
+    document.getElementById("news2title").innerHTML = arr[1].title;
+    document.getElementById("news3date").innerHTML = arr[2].pubDate;
+    document.getElementById("news3title").innerHTML = arr[2].title;
+  } catch {
+    console.log("cant parse news feed");
+  }
+};
--- README.md
+++ README.md
@@ -7,16 +7,18 @@ Your task is to build a responsive HTML page based on the .psd file provided her
 The task's goal is to show your core HTML/CSS skills. CSS frameworks like Flexbox and Bootstrap are ok, but large JS frameworks like React/Vue/Angular are not allowed. We hope to find the following things from your work:
 
 Basic skills:
-•	HTML+CSS
-•	The page is responsive on different devices
+• HTML+CSS
+• The page is responsive on different devices
 
 Extra points (Javascript/RSS/parallax):
-•	Main image with parallax Latest News section of the RSS feed: https://www.lianatech.com/resources/blog.rss (For CORS issues you can use https://code4developers.com/cors-anywhere/)
-•	Clients, Employees and Users sections with animated numbers
-•	Newsletter subscription and thank you notification
-•	Other skills may be demonstrated as well
+• Main image with parallax Latest News section of the RSS feed: https://www.lianatech.com/resources/blog.rss
+(For CORS issues you can use https://code4developers.com/cors-anywhere/)
+• Clients, Employees and Users sections with animated numbers
+• Newsletter subscription and thank you notification
+• Other skills may be demonstrated as well
+
 - -
 
-Personal notes
+Personal notes:
 icons from https://icons8.com/icon/set/popular/small
 images from https://www.pexels.com/fi-fi/
